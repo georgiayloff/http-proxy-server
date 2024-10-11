a79b51195c4727a2c33b1fb04f7205414fe4dc0d
Georgi Aylov <georgi.aylov@siemens.com>
2024-10-11 17:58:22 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# Http proxy server
+
+A simple http proxy server that serves static files for a specific destination
+and can proxy to other servers based on a relative path
+
+## Installation
+
+```shell
+npm install --save-dev @georgiayloff/http-proxy-server
+```
+
+or
+
+```shell
+yarn add -D @georgiayloff/http-proxy-server
+```
+
+## Usage
+
+```shell
+npm http-proxy-server <path-to-config>
+```
---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const { createProxyMiddleware } = require("http-proxy-middleware");
+const path = require("path");
+
+const createServer = (config) => {
+  const appDir = config.dist || path.join(process.cwd(), "dist");
+  const port = config.port || 4200;
+  const proxies = config.proxies || [];
+
+  const app = express();
+
+  app.use("/", express.static(appDir));
+
+  proxies.forEach((proxy) => {
+    app.use(
+      proxy.path,
+      createProxyMiddleware({
+        target: proxy.target + proxy.path,
+        changeOrigin: proxy.changeOrigin || false,
+        pathRewrite: proxy.pathRewrite,
+      })
+    );
+  });
+
+  app.listen(port, () => {
+    console.log(`Proxy server running on http://localhost:${port}`);
+  });
+};
+
+module.exports = createServer;
